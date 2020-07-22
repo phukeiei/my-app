0c7c0d54c342f1fe165c9d320ee20bb9d5e8f44d
phukeiei <34035927+phukeiei@users.noreply.github.com>
2020-07-22 10:06:05 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Phurk check Vue.js App"/>
+    <HelloWorld msg="Welcome to Phurk check asdsa Vue.js App"/>
   </div>
 </template>
 
